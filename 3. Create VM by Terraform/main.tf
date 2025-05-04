5cea2331536cf8869de98650e94f0972bfc113bd
David Abdallah <71341750+davabdallah@users.noreply.github.com>
2025-05-04 14:17:43 +0300

    Update main.tf

--- 3. Create VM by Terraform/main.tf
+++ 3. Create VM by Terraform/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "example" {
-    ami           = var.ami "ami-0f88e80871fd81e91" 
-    instance_type = var.instance_type "t2.micro"
+    ami           = var.image_id 
+    instance_type = var.instance_type
 
     tags = {
-      Name = "example-instance"
+      Name = "${var.project}-example"
     }
   }
