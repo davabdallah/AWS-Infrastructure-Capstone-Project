966f36c9e190a85cd646be14a11f492b8d154775
David Abdallah <71341750+davabdallah@users.noreply.github.com>
2025-05-04 14:15:49 +0300

    Create variables.tf

---
+++ 3. Create VM by Terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  type    = string
+}
+
+variable "image_id" {
+  type    = string
+}
+
+variable "instance_type" {
+  description = "The size of the VM instances."
+  type        = string
+}
